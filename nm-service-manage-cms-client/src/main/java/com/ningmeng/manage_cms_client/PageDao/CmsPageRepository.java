4eae01cba5a37481538f1d8e85e7f51c31e27e29
Mr.ceng <fc_learnt@163.com>
2020-02-14 21:33:24 +0800

    柠檬项目2/14

---
+++ nm-service-manage-cms-client/src/main/java/com/ningmeng/manage_cms_client/PageDao/CmsPageRepository.java
@@ -0,0 +1,5 @@
+package com.ningmeng.manage_cms_client.PageDao;
+
+public interface CmsPageRepository extends MongoRepository<CmsPage,String> {
+
+}
---
+++ nm-service-manage-cms-client/src/main/java/com/ningmeng/manage_cms_client/PageDao/CmsSiteRepository.java
@@ -0,0 +1,5 @@
+package com.ningmeng.manage_cms_client.PageDao;
+
+public interface CmsSiteRepository extends MongoRepository<CmsSite,String> {
+    
+}
