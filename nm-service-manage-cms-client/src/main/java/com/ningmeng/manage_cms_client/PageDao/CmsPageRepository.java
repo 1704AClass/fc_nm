5a3629f07d807e2568be786433a80e86caac23eb
Mr.ceng <fc_learnt@163.com>
2020-02-15 20:58:59 +0800

    在线教育2-15

--- nm-service-manage-cms-client/src/main/java/com/ningmeng/manage_cms_client/PageDao/CmsPageRepository.java
+++
@@ -1,5 +0,0 @@
-package com.ningmeng.manage_cms_client.PageDao;
-
-public interface CmsPageRepository extends MongoRepository<CmsPage,String> {
-
-}
--- nm-service-manage-cms-client/src/main/java/com/ningmeng/manage_cms_client/PageDao/CmsSiteRepository.java
+++
@@ -1,5 +0,0 @@
-package com.ningmeng.manage_cms_client.PageDao;
-
-public interface CmsSiteRepository extends MongoRepository<CmsSite,String> {
-    
-}
