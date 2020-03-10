b1171a690216b72b1e4bcaabda8bba87c5c3e6b2
Mr.ceng <fc_learnt@163.com>
2020-03-10 22:13:48 +0800

    nm-3/10

--- nm-service-api/src/main/java/com/ningmeng/api/auth/AuthControllerApi.java
+++ nm-service-api/src/main/java/com/ningmeng/api/auth/AuthControllerApi.java
@@ -1,8 +1,18 @@
 package com.ningmeng.api.auth;
 
+import com.ningmeng.framework.domain.ucenter.request.LoginRequest;
+import com.ningmeng.framework.domain.ucenter.response.LoginResult;
+import com.ningmeng.framework.model.response.ResponseResult;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
-@Api(value = "认证授权测试",description = "认证授权的方案")
+@Api(value = "用户认证授权测试",description = "用户认证授权的方案")
 public interface AuthControllerApi {
+
+    @ApiOperation("登录")
+    public LoginResult login(LoginRequest loginRequest);
+
+    @ApiOperation("退出")
+    public ResponseResult logout();
 
 }
